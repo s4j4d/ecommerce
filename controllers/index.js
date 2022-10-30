0057b085fe4dbfb335e459d6401a9da2077fb08a
Elhamto <90083067+Elhamto@users.noreply.github.com>
2022-10-31 01:03:59 +0330

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = {
-    cartController : require("./cart.controller"),
-    userController : require("./user.controller"),
-    productController : require("./product.controller"),
-}
