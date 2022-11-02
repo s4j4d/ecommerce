1ae550b77eb9485e43582aaf6ec4f4e563151416
Elhamto <elham.toloeii@gmail.com>
2022-11-02 19:32:51 +0330

    utils

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
-const config = require('./database/config/config');
-
+const config = require('./config/config.js')
 
 class Utils {
 
@@ -8,9 +7,17 @@ class Utils {
 
         return token = jwt.sign({ username: userObj.username }, config.secretkey, { algorithm: 'HS256' }, { expiresIn: '24h' })
     }
+
+    randStringGenerator(length) {
+        var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+        var charLength = chars.length;
+        var result = '';
+        for (var i = 0; i < length; i++) {
+            result += chars.charAt(Math.floor(Math.random() * charLength));
+        }
+        return result;
+    }
 }
-
-
 
 
 module.exports = new Utils() 
